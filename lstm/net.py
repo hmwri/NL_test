080c91b2450f4f82f1c8d038c92e5c971232a734
taiyuHonma <31509007+hmwri@users.noreply.github.com>
2022-11-09 19:56:54 +0900

    LSTM_3の実装

--- lstm/net.py
+++ lstm/net.py
@@ -35,3 +35,26 @@ class LSTM_NET_2(nn.Module):
         v = self.l1(y2)
         return v, hidden1, hidden2
 
+class LSTM_NET_3(nn.Module):
+    def __init__(self, n_embed, n_hidden, n_vocab):
+        super(LSTM_NET_3, self).__init__()
+        self.n_hidden = n_hidden
+        self.n_vocab = n_vocab
+        self.embedding = nn.Embedding(n_vocab, n_embed)
+        self.drop1 = nn.Dropout(0.5)
+        self.drop2 = nn.Dropout(0.5)
+        self.drop3 = nn.Dropout(0.5)
+        self.lstm1 = nn.LSTM(n_embed, n_hidden, batch_first=True)
+        self.lstm2 = nn.LSTM(n_hidden, n_hidden, batch_first=True)
+        self.l1 = nn.Linear(n_hidden, n_vocab)
+        self.l1.weight = self.embedding.weight
+
+    def forward(self, target, hidden1, hidden2):
+        e = self.embedding(target)
+        e = self.drop1(e)
+        y1, hidden1 = self.lstm1(e) if hidden1 is None else self.lstm1(e, hidden1)
+        y1 = self.drop2(y1)
+        y2, hidden2 = self.lstm2(y1) if hidden2 is None else self.lstm1(y1, hidden2)
+        y2 = self.drop3(y2)
+        v = self.l1(y2)
+        return v, hidden1, hidden2
--- showpickle.py
+++ showpickle.py
@@ -16,11 +16,15 @@ data1 = load_picke("/Users/taiyuu/Downloads/lstm_trainer_2_short_info.pkl")
 print(data1)
 data2 = load_picke("/Users/taiyuu/Downloads/lstm_trainer_2layer_info.pkl")
 print(data1)
+data3 = load_picke("/Users/taiyuu/Downloads/lstm_trainer_2layer_h100_info.pkl")
+print(data1)
 
 history_train1 = data1["history_train"]
 history_eval1 = data1["history_eval"]
 history_train2 = data2["history_train"]
 history_eval2 = data2["history_eval"]
+history_train3 = data3["history_train"]
+history_eval3 = data3["history_eval"]
 
-plot_history(history_train1,history_train2)
-plot_history(history_eval1,history_eval2)
+plot_history(history_train1,history_train2,history_train3)
+plot_history(history_eval1,history_eval2,history_eval3)
