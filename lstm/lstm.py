99aff5d351fd1998aa23e8a9a8f9716ac53b9654
taiyuHonma <31509007+hmwri@users.noreply.github.com>
2022-11-09 19:25:46 +0900

    1層化

--- lstm/lstm.py
+++ lstm/lstm.py
@@ -70,7 +70,7 @@ H = 100
 V = len(word_to_id)
 epoch_num = 15
 
-Net = net.LSTM_NET_2(n_embed,H,V)
+Net = net.LSTM_NET(n_embed,H,V)
 # for x, y in tqdm(dl):
 #     break
 criterion = nn.CrossEntropyLoss()
@@ -88,7 +88,7 @@ criterion = nn.CrossEntropyLoss()
 # img.render("NeuralNet")
 
 optim = torch.optim.Adam(Net.parameters())
-trainer = LstmTrainer2(Net,criterion,optim,name="lstm_trainer_2layer_h100")
+trainer = LstmTrainer(Net,criterion,optim,name="lstm_trainer_1layer_h100")
 
 trainer.fit(epoch_num,dl,dl_eval)
 
